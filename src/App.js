0383de71076697c0b08791d0526d9553ca2f8de7
Pedro Araujo <pedro-foa@hotmail.com>
2019-06-28 12:07:41 -0300

    Trocar o App de uma function para um stateless

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Vai 15 dias de React!!!</h1>
-  );
-}
+const App = () => <h1>Vai 15 dias de React!!!</h1>
 
 export default App;
+
