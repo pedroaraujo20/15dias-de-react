fb8f81a71448ac828dbede87e7df77da38f1ca6d
Pedro Araujo <pedro-foa@hotmail.com>
2019-06-30 20:56:27 -0300

    Feito o desafio básico do dia 01

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import Squad from '../Square/index';
+import Square from '../Square/index';
 
 const Board = () => {
     return (
-        <div>
-            <Squad />
-            <Squad />
-            <Squad />
-            <Squad />
-            <Squad />
-            <Squad />
-            <Squad />
-            <Squad />
-            <Squad />
-        </div>       
+        <>
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+        </>       
     );
 }
 
