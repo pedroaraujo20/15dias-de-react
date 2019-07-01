779249ccbaa8cce5b2aef6c58ba3590cbaacec96
Pedro Araujo <pedro-foa@hotmail.com>
2019-07-01 12:06:19 -0300

    Feito alguns estilos

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Board from './components/Board/index.js';
 import NextPlayer from './components/Nextplayer/index';
+import GameWrapper from './components/GameWrapper/index.js';
 
 const App = () => {
     return (
-        <>
+        <GameWrapper>
             <NextPlayer />
             <Board qtd="9"/>            
-        </>
+        </GameWrapper>
     );
 }
 
